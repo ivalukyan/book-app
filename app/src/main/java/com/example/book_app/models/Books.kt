e37ee8468bb6995772107c4c0313a61cf6e71dee
ivalukyan <ivalukyanov@gmail.com>
2024-05-10 15:13:18 +0300

    add new models

---
+++ app/src/main/java/com/example/book_app/models/Books.kt
@@ -0,0 +1,8 @@
+package com.example.book_app.models
+
+class Books {
+    var id: Long? = null
+    var name: String = ""
+    var author: String = ""
+    var description: String? = ""
+}
---
+++ app/src/main/java/com/example/book_app/models/User.kt
@@ -0,0 +1,9 @@
+package com.example.book_app.models
+
+class User {
+    var id: Long? = null
+    var username: String = ""
+    var name: String = ""
+    var email: String = ""
+    var password: String = ""
+}
