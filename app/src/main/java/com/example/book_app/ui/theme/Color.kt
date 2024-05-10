e06d60630c2cdff6ca81a76b096c8dd3a251d393
ivalukyan <ivalukyanov@gmail.com>
2024-05-10 15:31:03 +0300

    add new colors and color to theme

--- app/src/main/java/com/example/book_app/ui/theme/Color.kt
+++ app/src/main/java/com/example/book_app/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.book_app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Светлая тема
+val BackGround = Color(0xFFFFFFFF)
+val TextSignIn = Color(0xFF000000)
+val TextInBox = Color(0xFF828282)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Темная тема
--- app/src/main/java/com/example/book_app/ui/theme/Theme.kt
+++ app/src/main/java/com/example/book_app/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    surface = BackGround,
+    primary = TextSignIn,
+    secondary = TextInBox
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    surface = BackGround,
+    primary = TextSignIn,
+    secondary = TextInBox
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
